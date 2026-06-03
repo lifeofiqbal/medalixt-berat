caa76113b6e4f851dd05ff7bb4abd32fbe55b542
lifeofiqbal <59346706+lifeofiqbal@users.noreply.github.com>
2026-06-03 11:06:02 +0700

    Refactor player weight management in SQL
    Removed manual addition of maxweight column and created player_custom_weight table.

--- medalixt-berat.sql
+++ medalixt-berat.sql
@@ -1,2 +1,7 @@
-ALTER TABLE players
-ADD COLUMN maxweight INT DEFAULT 50000;
+-- SQL INI GA PERLU DI TAMBAHKAN SECARA MANUAL KARENA AKAN BERJALAN OTOMATIS APABILA medalixt-berat SUDAH DI CFG
+
+CREATE TABLE player_custom_weight (
+    identifier VARCHAR(60),  -- license:xxxx
+    max_weight INT,           -- dalam gram
+    updated_at TIMESTAMP
+)
